742daba1e88f717b52031dec05231889ded40468
Elijah Smith <elijahsmith.m@gmail.com>
2022-05-16 18:14:39 -0500

    commit tree 0c916a968e49d3a767f4c2d2e26aac167e8904f7

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'plants/index'
+  get 'plants/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :plants, only: [:index, :show, :create]
 end
